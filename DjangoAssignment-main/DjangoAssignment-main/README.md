46ac54f4a2f2a858fd554ba2104b73f3d8343d24
Atharva-2157 <64467337+Atharva-2157@users.noreply.github.com>
2022-07-15 11:08:25 +0530

    Delete DjangoAssignment-main directory

--- DjangoAssignment-main/DjangoAssignment-main/README.md
+++
@@ -1,1 +0,0 @@
-# DjangoAssignment
--- DjangoAssignment-main/DjangoAssignment-main/studentData/app1/admin.py
+++
@@ -1,5 +0,0 @@
-from atexit import register
-from django.contrib import admin
-from app1.models import student
-# Register your models here.
-admin.site.register(student)
